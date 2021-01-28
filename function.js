366542ea43f5642b3c3e302dac708485f441342a
jannath <jannathsyeda7@gmail.com>
2021-01-28 15:23:08 +0600

    eventListener and bubble

--- function.js
+++ function.js
@@ -52,3 +52,29 @@ console.log(array_All(array));
     callBack(Name,Age, goHome);
     callBack('mita',10, sleep);
 
+    //arguments (arguments is array like object,only work in any function,push pop and any other library function is not allow with arguments)
+//if u want access all parameter which u send ,then use arguments 
+    function add(n1,n2)
+    {
+        console.log(arguments);
+        sum=n1+n2;
+        return sum;
+    }
+
+    var result=add(5,5,5,5,5);
+    console.log(result);
+
+//sun+arguments
+
+     function sumFunction(n){
+        var sum=0;
+        for (let i = 0; i < arguments.length; i++) {
+            const element = arguments[i];
+            sum=sum+element;
+        }
+        return sum;
+     }
+
+     var numbers=sumFunction(1,3,2,4,5,4,3,2,1,3,2,4,5,4,3,2);
+     console.log(numbers);
+    
